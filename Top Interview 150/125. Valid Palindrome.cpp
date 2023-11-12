98a21d3f4474facc177cc7dcefd6ab3d41d998fb
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2023-11-12 15:00:46 +0600

    Create 125. Valid Palindrome.cpp

---
+++ Top Interview 150/125. Valid Palindrome.cpp
@@ -0,0 +1,20 @@
+problem link : https://leetcode.com/problems/valid-palindrome/description/?envType=study-plan-v2&envId=top-interview-150
+class Solution {
+public:
+    bool isPalindrome(string s) {
+         string p,t;
+         transform(s.begin(), s.end(), s.begin(), ::tolower);
+        for(int i=0;i<s.size();i++){
+            if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z')){
+                p.push_back(s[i]);
+            }
+        }
+        t=p;
+        cout<<t<<endl;
+        reverse(t.begin(),t.end());
+        if(t==p){
+            return true;
+        }
+        return false;
+    }
+};
