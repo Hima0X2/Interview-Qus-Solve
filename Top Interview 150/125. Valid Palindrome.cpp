ff433729e1c3b9c5629933a0f147229ba7e764de
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2023-11-12 15:02:21 +0600

    Update 125. Valid Palindrome.cpp

--- Top Interview 150/125. Valid Palindrome.cpp
+++ Top Interview 150/125. Valid Palindrome.cpp
@@ -5,7 +5,7 @@ public:
          string p,t;
          transform(s.begin(), s.end(), s.begin(), ::tolower);
         for(int i=0;i<s.size();i++){
-            if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z')){
+            if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z')||(s[i]>='0'&&s[i]<='9')){
                 p.push_back(s[i]);
             }
         }
