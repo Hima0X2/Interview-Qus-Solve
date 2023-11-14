0e7891472e9d424387bba3bb7a190b1b5d98e829
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2023-11-14 22:40:14 +0600

    Create 1. Two Sum.cpp

---
+++ Top Interview 150/1. Two Sum.cpp
@@ -0,0 +1,29 @@
+problem link : https://leetcode.com/problems/two-sum/description/?envType=study-plan-v2&envId=top-interview-150
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>> v;
+        for(int i=0;i<nums.size();i++)
+            v.push_back({nums[i],i});
+        sort(v.begin(),v.end());
+        int i=0;
+        int j=v.size()-1;
+        int idx1=0;
+        int idx2=0;
+        while(i<j)
+        {
+            if(v[i].first+v[j].first==target)
+            {
+                idx1=v[i].second;
+                idx2=v[j].second;
+                break;
+                
+            }
+            if(v[i].first+v[j].first<target)
+                i++;
+            if(v[i].first+v[j].first>target)
+                j--;
+    }
+        return {idx1,idx2};
+    }
+};
