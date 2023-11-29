d8b91403c0d907fe8a99d14de373f041e05644df
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2023-11-29 13:56:51 +0600

    Update 172. Factorial Trailing Zeroes.cpp

--- Top Interview 150/172. Factorial Trailing Zeroes.cpp
+++ Top Interview 150/172. Factorial Trailing Zeroes.cpp
@@ -1,1 +1,11 @@
-a
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int sum=0;
+       while(n>0){
+           sum=sum+n/5;
+           n=n/5;
+       }
+       return sum;
+    }
+};
