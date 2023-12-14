2ee2d29d6f405a6b2304e3c1043010c6c0063132
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2023-12-14 16:40:09 +0600

    Update 49. Group Anagrams.cpp

--- Top Interview 150/49. Group Anagrams.cpp
+++ Top Interview 150/49. Group Anagrams.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<pair<string, string>>v;
+    for(int i=0; i<strs.size(); i++){
+        string s=strs[i];
+        sort(s.begin(), s.end());
+        v.push_back({s, strs[i]});
+    }
+    sort(v.begin(), v.end());
+    unordered_map<string, vector<string>>m;
+    for(int i=0; i<v.size(); i++){
+        m[v[i].first].push_back(v[i].second);
+    }
+    vector<vector<string>>ans;
+    for(auto it: m){
+        ans.push_back(it.second);
+    }
+    return ans;
+    }
+};
