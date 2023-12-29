d94c0a6192b8ddd9c6a3c4b578ef5bc0fbe2712a
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2023-12-29 18:17:41 +0600

    Update 20. Valid Parentheses.cpp

--- Top Interview 150/20. Valid Parentheses.cpp
+++ Top Interview 150/20. Valid Parentheses.cpp
@@ -1,1 +1,41 @@
-
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(st.empty()){
+                if(s[i]=='('||s[i]=='{'||s[i]=='['){
+                    st.push(s[i]);
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                if(s[i]=='('||s[i]=='{'||s[i]=='['){
+                    st.push(s[i]);
+                }
+                else{
+                    if(st.top()=='('&&s[i]==')'){
+                        st.pop();
+                    }
+                    else if(st.top()=='{'&&s[i]=='}'){
+                        st.pop();
+                    }
+                    else if(st.top()=='['&&s[i]==']'){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+        if(st.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
