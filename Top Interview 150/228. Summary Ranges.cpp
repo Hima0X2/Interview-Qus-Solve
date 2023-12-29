77cecced66ef60533dcdc9a876fb53b329a0cd31
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2023-12-29 17:51:42 +0600

    Update 228. Summary Ranges.cpp

--- Top Interview 150/228. Summary Ranges.cpp
+++ Top Interview 150/228. Summary Ranges.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& v) {
+        vector<string> result;
+        string s="";
+        for(int i=0;i<v.size();i++){
+              int ans=v[i];
+            while(i+1<v.size()&&v[i+1]==v[i]+1){
+                i++;
+            }
+            if(ans!=v[i]){
+                result.push_back(to_string(ans)+"->"+to_string(v[i]));
+            }
+            else{
+                result.push_back(to_string(ans));
+            }
+        }
+        return result;
+    }
+};
