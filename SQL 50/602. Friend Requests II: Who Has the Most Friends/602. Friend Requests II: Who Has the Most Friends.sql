5b8e4d915e2cffee7213cc8e954a760e45d86d2d
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-01-08 15:49:49 +0600

    Update 602. Friend Requests II: Who Has the Most Friends.sql

--- SQL 50/602. Friend Requests II: Who Has the Most Friends/602. Friend Requests II: Who Has the Most Friends.sql
+++ SQL 50/602. Friend Requests II: Who Has the Most Friends/602. Friend Requests II: Who Has the Most Friends.sql
@@ -1,1 +1,9 @@
-
+select id,count(frd_id) as num
+from(
+    select requester_id as id, accepter_id as frd_id from RequestAccepted
+    union 
+    select accepter_id as id, requester_id as frd_id
+    from RequestAccepted
+) as friend
+group by id
+order by num desc limit 1;
