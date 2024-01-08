504743b3d4e973f16d734ef1a209b19e26741e0e
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-01-08 16:07:33 +0600

    Update 1280. Students and Examinations.sql

--- SQL 50/1280. Students and Examinations/1280. Students and Examinations.sql
+++ SQL 50/1280. Students and Examinations/1280. Students and Examinations.sql
@@ -1,1 +1,10 @@
-
+SELECT
+    s.student_id,
+    s.student_name,
+    sub.subject_name,
+    (SELECT COUNT(*) FROM Examinations e WHERE e.student_id = s.student_id AND e.subject_name = sub.subject_name) AS attended_exams
+FROM
+    Students s,
+    Subjects sub
+ORDER BY
+    s.student_id, sub.subject_name;
