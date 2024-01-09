f19e298c29c2e5b6de04b7b7b35439f7aa1860d7
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-01-09 17:12:36 +0600

    Update 55. Jump Game.cpp

--- Top Interview 150/55. Jump Game.cpp
+++ Top Interview 150/55. Jump Game.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>sum){
+                return false;
+            }
+            sum=max(sum,i+nums[i]);
+        }
+        return true;
+    }
+};
