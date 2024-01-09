b926cef94a4ffb9455448b97ade4a375cc61f7b3
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-01-09 18:43:01 +0600

    Update 14. Longest Common Prefix.cpp

--- Top Interview 150/14. Longest Common Prefix.cpp
+++ Top Interview 150/14. Longest Common Prefix.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
 
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        string result;
+        string a=strs[0],b=strs[strs.size()-1];
+        for(int i=0;i<a.size();i++){
+            if(a[i]==b[i]){
+                result.push_back(a[i]);
+            }
+            else{
+                break;
+            }
+        }
+        return result;
+    }
+};
