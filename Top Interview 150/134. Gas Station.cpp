7d389aece46c9f2857aedd1b203d1417e7b87d90
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-01-12 14:08:23 +0600

    Update 134. Gas Station.cpp

--- Top Interview 150/134. Gas Station.cpp
+++ Top Interview 150/134. Gas Station.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n=gas.size(),total_gas=0,total_cost=0,current_gas=0,pos=0;
+        for(int i=0;i<n;i++){
+            total_gas=total_gas+gas[i];
+            total_cost=total_cost+cost[i];
+            current_gas=current_gas+(gas[i]-cost[i]);
+            if(current_gas<0){
+                pos=i+1;
+                current_gas=0;
+            }
+        }
+        if(total_gas<total_cost){
+            return -1;
+        }
+        return pos;
+    }
+};
