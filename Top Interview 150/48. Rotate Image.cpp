90d07a862825f990243c7bca4cf70f13e25b6141
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-01-21 00:57:02 +0600

    Update 48. Rotate Image.cpp

--- Top Interview 150/48. Rotate Image.cpp
+++ Top Interview 150/48. Rotate Image.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& a) {
+        int n = a.size();
+   vector<vector<int>> result(n,vector<int>(n, 0));
 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                result[i][j]=a[j][i];
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(result[i].begin(),result[i].end());
+        }
+       for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                a[i][j]=result[i][j];
+            }
+        }
+    }
+};
