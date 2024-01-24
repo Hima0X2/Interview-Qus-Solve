1b0bd3133b506ca02f655a467d3c40c4da561b05
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-01-24 21:01:58 +0600

    Update 70. Climbing Stairs.cpp

--- Top Interview 150/70. Climbing Stairs.cpp
+++ Top Interview 150/70. Climbing Stairs.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1||n==2){
+            return n;
+        }
+        int one=1,two=2,c;
+        for(int i=3;i<=n;i++){
+            c=one+two;
+            one=two;
+            two=c;
+        }
+        return c;
+    }
+};
