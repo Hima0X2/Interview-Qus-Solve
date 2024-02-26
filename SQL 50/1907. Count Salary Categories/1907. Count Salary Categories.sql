c25c984e18706976f3ac80f5d47fd7d0c5e0e7e3
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-02-26 11:47:36 +0600

    Update 1907. Count Salary Categories.sql

--- SQL 50/1907. Count Salary Categories/1907. Count Salary Categories.sql
+++ SQL 50/1907. Count Salary Categories/1907. Count Salary Categories.sql
@@ -1,1 +1,6 @@
+(select "Low Salary" as category,count(*) as accounts_count from Accounts where income<20000)
+union all
+(select "Average Salary" as category,count(*) as accounts_count from Accounts where income between 20000 and 50000)
+union all
+(select "High Salary" as category,count(*) as accounts_count from Accounts where income>50000)
 
