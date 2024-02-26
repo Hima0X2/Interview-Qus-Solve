cc3c90977ad416eb1c9196f8d0d30f1737ad89ad
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-02-26 22:19:28 +0600

    Update 1341. Movie Rating.sql

--- SQL 50/1341. Movie Rating/1341. Movie Rating.sql
+++ SQL 50/1341. Movie Rating/1341. Movie Rating.sql
@@ -1,1 +1,14 @@
+(SELECT name AS results
+FROM MovieRating JOIN Users USING(user_id)
+GROUP BY name
+ORDER BY COUNT(*) DESC, name
+LIMIT 1)
 
+UNION ALL
+
+(SELECT title AS results
+FROM MovieRating JOIN Movies USING(movie_id)
+WHERE EXTRACT(YEAR_MONTH FROM created_at) = 202002
+GROUP BY title
+ORDER BY AVG(rating) DESC, title
+LIMIT 1)
