6bf5fb0c31a6edda1468a5663bb9cd206c1a139b
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-03-10 13:50:03 +0600

    Update 1193. Monthly Transactions I.sql

--- SQL 50/1193. Monthly Transactions I/1193. Monthly Transactions I.sql
+++ SQL 50/1193. Monthly Transactions I/1193. Monthly Transactions I.sql
@@ -1,1 +1,3 @@
-
+SELECT  SUBSTR(trans_date,1,7) as month, country, count(id) as trans_count, SUM(CASE WHEN state = 'approved' then 1 else 0 END) as approved_count, SUM(amount) as trans_total_amount, SUM(CASE WHEN state = 'approved' then amount else 0 END) as approved_total_amount
+FROM Transactions
+GROUP BY month, country
