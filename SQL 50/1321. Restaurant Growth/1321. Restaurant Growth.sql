a4ba63ad9392922f9527f8bf51ea956c9548cdfc
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-03-10 11:57:52 +0600

    Update 1321. Restaurant Growth.sql

--- SQL 50/1321. Restaurant Growth/1321. Restaurant Growth.sql
+++ SQL 50/1321. Restaurant Growth/1321. Restaurant Growth.sql
@@ -1,1 +1,9 @@
-
+SELECT visited_on, amount, round(amount/7, 2) average_amount
+FROM
+(
+    SELECT DISTINCT visited_on,
+    sum(amount) OVER (ORDER BY visited_on range BETWEEN interval 6 day preceding AND current row) amount,
+    MIN(visited_on) OVER() 1ST_date
+    FROM customer
+) t
+WHERE visited_on >= 1st_date + 6
