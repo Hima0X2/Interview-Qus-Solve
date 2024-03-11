1b0ac54a0067a857c7c101010f6448dfa2cf0912
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-03-11 21:40:03 +0600

    Update 6. Zigzag Conversion.cpp

--- Top Interview 150/6. Zigzag Conversion.cpp
+++ Top Interview 150/6. Zigzag Conversion.cpp
@@ -1,1 +1,33 @@
-
+class Solution {
+public:
+    string convert(string s, int n) {
+        long long int t,i,row=1;
+        string v[1000],p="";
+     if(n==1){
+          return s;
+     }
+     else{
+               bool ok;
+          for(i=0;i<s.size();i++){
+                    v[row]=v[row]+s[i];
+              if(row==1){
+               ok=true;
+              }
+              else if(row==n){
+               ok=false;
+              }
+              if(ok){
+               row++;
+              }
+              else{
+               row--;
+              }
+          }
+    }
+       for(i=1;i<=n;++i){
+           p=p+v[i];
+          }
+        return p;
+    }
+        
+};
