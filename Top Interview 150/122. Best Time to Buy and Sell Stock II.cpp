7d0ea85034e5514e0ae542c91b4217eff989993a
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-03-11 21:40:46 +0600

    Update 122. Best Time to Buy and Sell Stock II.cpp

--- Top Interview 150/122. Best Time to Buy and Sell Stock II.cpp
+++ Top Interview 150/122. Best Time to Buy and Sell Stock II.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& p) {
+        int ans=0;
+        for(int i=1;i<p.size();i++){
+            if(p[i]-p[i-1]>0){
+                ans=ans+p[i]-p[i-1];
+            }
+        }
+        return ans;
+    }
+};
