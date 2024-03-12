59a610a72d9291388b605739662036bcd4625b39
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-03-12 20:47:31 +0600

    Update 11. Container With Most Water.cpp

--- Top Interview 150/11. Container With Most Water.cpp
+++ Top Interview 150/11. Container With Most Water.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int maxArea(vector<int>& a) {
+        int i=0,j=a.size()-1,ans=0;
+        while(i<j){
+            if(a[i]>a[j]){
+                ans=max(ans,(j-i)*a[j]);
+                j--;
+            }
+            else{
+                ans=max(ans,(j-i)*a[i]);
+                i++;
+            }
+        }
+        return ans;
+    }
+};
