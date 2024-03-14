02f6d0ad9fbb50338e92ed44282eacf7ded44eca
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-03-14 11:36:08 +0600

    Update 74. Search a 2D Matrix.cpp

--- Top Interview 150/74. Search a 2D Matrix.cpp
+++ Top Interview 150/74. Search a 2D Matrix.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(int x=0;x<matrix.size();x++){
+            int i=0,j=matrix[x].size()-1;
+            while(i<=j){
+            int mid=(i+j)/2;
+            if(target<matrix[x][mid]){
+                j=(mid-1);
+            }
+            else if(target>matrix[x][mid]){
+                i=(mid+1);
+            }
+            else{
+                return true;
+            }
+        }
+        }
+        return false;
+    }
+};
