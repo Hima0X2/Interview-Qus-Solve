98eef5e5b5005bfde5373d3596efd3ac169d71b2
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-03-14 11:35:39 +0600

    Update 162. Find Peak Element.cpp

--- Top Interview 150/162. Find Peak Element.cpp
+++ Top Interview 150/162. Find Peak Element.cpp
@@ -1,1 +1,28 @@
-
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int i,j=0;
+        if(nums.size()==1){
+                j=0;
+            }
+        else if(nums.size()==2){
+            if(nums[0]>=nums[1]){
+               j=0;
+            }
+            else{
+              j=1;  
+            }
+        }
+        else{
+            if(nums[0]>nums[1]) j=0;
+        for(i=2;i<nums.size();i++){
+          if(nums[i]<nums[i-1]&&nums[i-2]<nums[i-1]){
+              j=i-1;
+               break;
+          }
+     }
+            if(nums[nums.size()-1]>nums[nums.size()-2]) j=nums.size()-1;
+        }
+        return j;
+    }
+};
