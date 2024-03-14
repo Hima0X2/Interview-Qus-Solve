8ba8eb2fbbb8042c3cebfb514cf4a7bc250d7d7c
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-03-14 11:46:19 +0600

    Update 56. Merge Intervals.cpp

--- Top Interview 150/56. Merge Intervals.cpp
+++ Top Interview 150/56. Merge Intervals.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+         vector<vector<int>> m;
+        if(intervals.size()==0){
+            return m;
+        }
+        sort(intervals.begin(),intervals.end());
+        vector<int> temp=intervals[0];
+        for(auto i:intervals){
+            if(i[0]<=temp[1]){
+                temp[1]=max(temp[1],i[1]);
+            }
+            else{
+                m.push_back(temp);
+                temp=i;
+            }
+        }
+        m.push_back(temp);
+        return m;
+    }
+};
