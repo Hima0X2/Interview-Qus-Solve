ad7db0d96c9b3eeab4ac127243ab830181a2086e
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-03-16 14:07:18 +0600

    Update 274. H-Index.cpp

--- Top Interview 150/274. H-Index.cpp
+++ Top Interview 150/274. H-Index.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.rbegin(),citations.rend());
+        int i=0;
+        while(i<citations.size()&&citations[i]>i){
+            i++;
+        }
+        return i;
+    }
+};
