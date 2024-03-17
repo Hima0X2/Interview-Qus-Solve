30a334a9614639bd33ef44d9f97b9f0f449e54a9
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-03-17 12:28:36 +0600

    Create 275. H-Index II.cpp

---
+++ Top Interview 150/275. H-Index II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int hIndex(vector<int>& c) {
+        int n = c.size();
+        if(!n) return 0;
+        int low =0,high=n-1;
+        int ans = 0;
+        while(low<=high){
+            int mid  = low + (high-low)/2;
+            if(c[mid] >= n-mid){
+                ans = n-mid;
+                high = mid - 1;
+            }else{
+                low  = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+};
