452d704c728bf3422ac074ebb6f0eb195988aa5a
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-04-27 12:44:39 +0600

    Update 36. Valid Sudoku.cpp

--- Top Interview 150/36. Valid Sudoku.cpp
+++ Top Interview 150/36. Valid Sudoku.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<int,unordered_map<char,int>> board_map;
+        for(auto i = 0; i<9; i++){
+            for(auto j = 0; j<9; j++){
+                char cell = board[i][j];
+                if(cell != '.'){
+                    if(board_map[i][cell]++ > 0) return false;
+                    if(board_map[j+9][cell]++ > 0) return false;
+                    if(board_map[27 + (i/3)*3 +j/3][cell]++ > 0) return false;
+                    }
+            }
+        }
+        return true;
+        
+    }
+};
